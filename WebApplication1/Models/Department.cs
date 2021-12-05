40a375308fc1d2e1a679438fb04a18b13698e557
Fernando <kl.fedor@mail.ru>
2021-12-05 12:49:13 +0300

    Employee,DepartmentController; Models Department, Employee

---
+++ WebApplication1/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    //класс отдел
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    //класс сотрудник
+    public class Employee
+    {
+        public int EmployeeId { get; set; } //id сотрудника
+        public string EmployeeName { get; set; } //имя сотрудника
+        public string Department { get; set; } //отдел
+        public string DateOfJoining { get; set; } //дата начала работы
+        public string PhotoFileName { get; set; } //фото
+    }
+}
